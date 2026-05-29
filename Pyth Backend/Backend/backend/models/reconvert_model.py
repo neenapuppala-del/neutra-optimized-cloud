3f86b841cf909bad06147f63676a21cff40c83a3
Neena <neena.puppala@gmail.com>
2026-05-29 15:21:55 +0530

    Upgrade tensorflow runtime

--- Pyth Backend/Backend/backend/models/reconvert_model.py
+++ Pyth Backend/Backend/backend/models/reconvert_model.py
@@ -1,21 +1,22 @@
 import tensorflow as tf
 
-print("TensorFlow version:", tf.__version__)
+print("TF:", tf.__version__)
 
-model = tf.keras.models.load_model("food_model_final.h5")
+model = tf.keras.models.load_model(
+    "food_model_final.h5",
+    compile=False
+)
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-# IMPORTANT: compatible ops
+converter.optimizations = []
 converter.target_spec.supported_ops = [
     tf.lite.OpsSet.TFLITE_BUILTINS
 ]
 
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-
 tflite_model = converter.convert()
 
-with open("food_model_render.tflite", "wb") as f:
+with open("food_model_render_v2.tflite", "wb") as f:
     f.write(tflite_model)
 
-print("Done. Generated food_model_render.tflite")
+print("DONE")
